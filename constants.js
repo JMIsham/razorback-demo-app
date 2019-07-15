70753d00329a12e7d86a635f56a55c6dc9670b0e
JMIsham <isham.mohamed@syscolabs.com>
2019-07-15 14:30:33 +0530

    more constants

--- constants.js
+++ constants.js
@@ -1,5 +1,7 @@
 export const PO_CLI = "/home/ubuntu/razorback-sabre-contract/po/cli/target/debug/po-cli"
 export const SABRE_CLI = "/home/ubuntu/sabre-cli/sabre"
+export const SABRE_CLI_API = "http://127.0.0.1:5002"
+export const SAWTOOTH_REST_API = "http://127.0.0.1:8008"
 
 export const EXEC_COMMAND = ` exec --contract purchase-order:1.0 --payload payload --inputs  000008 --outputs  000008 --url http://127.0.0.1:8008`
 export const PAYLOAD_PATH = "/root/payload/payload"
